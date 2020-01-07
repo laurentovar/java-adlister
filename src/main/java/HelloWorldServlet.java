bc62bb3bb38c9bf165742ff3729bb1c9761a7985
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-01-07 12:07:36 -0600

    servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,15 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
+//make sure you put hello-world after the / in the local host link!
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
     }
-
 }
---
+++ src/main/java/PageCounter.java
@@ -0,0 +1,34 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//page that goes up one every time the page is viewed
+@WebServlet(name = "CountServlet", urlPatterns = "/page-counter")
+
+public class PageCounter extends HttpServlet {
+
+    // set counter
+    private static int counter = 0;
+
+    protected void doGet(HttpServletRequest servReq, HttpServletResponse serResp) throws ServletException, IOException {
+        String reset = servReq.getParameter("reset");
+
+
+        serResp.setContentType("text/html");
+
+        PrintWriter writer = serResp.getWriter();
+        counter++;
+
+        if (reset != null && reset.toLowerCase().equals("true")) {
+            counter = 1;
+
+            writer.println("<h2>You rest the count of the page!!</h2>");
+        }
+        writer.println("<h3>The Page count is: " + counter + "</h3>");
+    }
+}
